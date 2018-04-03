7fe0988da005aa4eb30a583550fe3a98b0832daa
Graham Cox <graham@grahamcox.co.uk>
2018-04-03 21:21:57 +0100

    Connect to the database on use

--- backend/todoDao.js
+++ backend/todoDao.js
@@ -1,35 +1,45 @@
 const MongoClient = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;
 
-const mongodb = new Promise((resolve, reject) => {
-    MongoClient.connect(process.env.MONGODB_URL, {
-        poolSize: 10
-    }, (err, client) => {
-        if (err) {
-            console.log('Error connecting to MongoDB');
-            console.log(err);
-            reject(err);
-        } else {
-            console.log('Connected to MongoDB');
-            resolve(client.db(process.env.MONGODB_DATABASE));
-        }
-    });
-});
+let mongodb;
+
+function getMongoConnection() {
+    if (!mongodb) {
+        mongodb = new Promise((resolve, reject) => {
+            MongoClient.connect(process.env.MONGODB_URL, {
+                poolSize: 10,
+                autoReconnect: true,
+                reconnectTries: 60,
+                reconnectInterval: 1000
+            }, (err, client) => {
+                if (err) {
+                    console.log('Error connecting to MongoDB');
+                    console.log(err);
+                    reject(err);
+                } else {
+                    console.log('Connected to MongoDB');
+                    resolve(client.db(process.env.MONGODB_DATABASE));
+                }
+            });
+        });
+    }
+    return mongodb;
+}
 
 function listTodos() {
-    return mongodb
+    return getMongoConnection()
         .then((db) => db.collection('todos'))
         .then((col) => col.find().toArray());
 }
 
 function getTodoById(id) {
-    return mongodb
+    return getMongoConnection()
         .then((db) => db.collection('todos'))
         .then((col) => col.findOne({_id: new ObjectID(id)}));
 }
 
 function createTodo(todo) {
-    return mongodb
+    return getMongoConnection()
         .then((db) => db.collection('todos'))
         .then((col) => col.insertOne({
             title: todo.title,
@@ -39,13 +49,13 @@ function createTodo(todo) {
 }
 
 function deleteTodo(id) {
-    return mongodb
+    return getMongoConnection()
         .then((db) => db.collection('todos'))
         .then((col) => col.findOneAndDelete({_id: new ObjectID(id)}));
 }
 
 function updateTodo(id, todo) {
-    return mongodb
+    return getMongoConnection()
         .then((db) => db.collection('todos'))
         .then((col) => col.findOneAndUpdate({_id: new ObjectID(id)}, {
             title: todo.title,
